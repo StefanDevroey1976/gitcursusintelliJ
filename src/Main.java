fe1af088a341e284fb511c1222ed5e21e0882e7e
SDevroey <stefan.devroey@capgemini.com>
2022-11-16 10:54:38 +0100

    Hello iedereen op master branch

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello wereld van local/Stefan");
+        System.out.println("Hello iedereen op master branch");
     }
 }
